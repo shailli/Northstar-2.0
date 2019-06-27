4e5ea7789aaf36cca8c1312d594844ad9acec010
Abhijeet Kumar <>
2019-06-27 12:24:15 +0530

    ios build issues

--- packages/mobile/src/components/login/login.test.js
+++ packages/mobile/src/components/login/login.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { configure, shallow } from 'enzyme';
 // setup file
-import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Login from '.';
 
