cac393df0edb8961b89ab6a7838f923833c1f94e
Abhishek P <abhishek.p@piktorlabs.com>
2019-06-30 17:22:02 +0530

    Hardcoded listing page

--- packages/mobile/src/components/chatlist/components/ListItem.js
+++ packages/mobile/src/components/chatlist/components/ListItem.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import { Avatar } from 'react-native-elements';
+import { containerStyle, titleStyle, messageStyle, textContainerStyle } from './ListItem.styles';
 
 const ListItem = (props) => {
-    const { name } = props;
+    const { name, message } = props.data;
     return (
-        <View>
-            <Avatar />
-            <Text>{name}</Text>
+        <View style={containerStyle}>
+            <Avatar
+                rounded
+                size={48}
+                title={name.charAt(0)}
+            />
+            <View style={textContainerStyle}>
+                <Text numberOfLines={1} style={titleStyle}>{name}</Text>
+                <Text numberOfLines={1} style={messageStyle}>{message}</Text>
+            </View>
         </View>
     );
 }
---
+++ packages/mobile/src/components/chatlist/components/ListItem.styles.js
@@ -0,0 +1,27 @@
+module.exports = {
+    containerStyle: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingTop: 10,
+        paddingBottom: 10,
+        paddingLeft: 10,
+        borderColor: '#000',
+        borderStyle: 'solid',
+        borderBottomWidth: 1
+    },
+    textContainerStyle: {
+        display: 'flex',
+        flex: 1,
+        paddingLeft: 10,
+        paddingRight: 10,
+    },
+    titleStyle: {
+        color: '#000',
+        fontSize: 20,
+    },
+    messageStyle: {
+        color: '#000',
+        fontSize: 16,
+    }
+};
