403c030e81b619018b76097f404f582522d28874
Abhishek P <abhishek.p@piktorlabs.com>
2019-07-01 11:57:23 +0530

    Linting + added jest to gitignore

--- packages/mobile/src/components/login/index.js
+++ packages/mobile/src/components/login/index.js
@@ -1,45 +1,27 @@
 import React, { useState } from 'react';
-import {
-    StyleSheet, Text, View, TouchableOpacity, TextInput, Button
-} from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput, Button } from 'react-native';
 export default function Login() {
   const [userName, setUserName] = useState('');
   const [password, setPassword] = useState('');
   const [isLoggedIn, setIsLoggedin] = useState(false);
 
-    handleUserIdChange = (name) => {
-        setUserName(name)
+  handleUserIdChange = name => {
+    setUserName(name);
+  };
+  handlePasswordChange = password => {
+    setPassword(password);
+  };
+  submitclick = () => {
+    if (userName == 'krishankantsinghal' && password == 'krishankant123') {
+      setIsLoggedin(true);
     }
-    handlePasswordChange = (password) => {
-        setPassword(password)
-    }
-    submitclick = () => {
-        if ((userName == "krishankantsinghal") && (password == "krishankant123")) {
-            setIsLoggedin(true);
-        }
-    }
-    
-    return (
-      <View>
-            <TextInput 
-                placeholder="Username"
-                onChangeText={this.handleUserIdChange}
-                name="username"
-            />
-            <TextInput 
-                placeholder="Password"
-                onChangeText={this.handlePasswordChange}
-                name="password"
-            />
-            {/* <input type="text" name="username" hint="username" onChange={this.handleUserIdChange} />
-            <input type="password" name="password" hint="password" onChange={this.handlePasswordChange} /> */}
-            {/* <button name="submit" onClick={this.submitclick}> Submit</button> */}
-            <Button type="submit"
-                onPress={this.submitclick}
-                onKeyPress={this.submitclick}
-                title='Login'
-            />
-        </View>
-    );
-};
-  
+  };
+
+  return (
+    <View>
+      <TextInput placeholder="Username" onChangeText={this.handleUserIdChange} name="username" />
+      <TextInput placeholder="Password" onChangeText={this.handlePasswordChange} name="password" />
+      <Button type="submit" onPress={this.submitclick} onKeyPress={this.submitclick} title="Login" />
+    </View>
+  );
+}
