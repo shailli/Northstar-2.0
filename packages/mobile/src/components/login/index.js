6e6130d39e0d9bd8c4de5d3e8a0356bdaba15ece
Ganapati Dattatraya Hegde <ganapati@piktorlabs.com>
2019-07-02 16:08:41 +0530

    Error fixed

--- packages/mobile/src/components/login/index.js
+++ packages/mobile/src/components/login/index.js
@@ -11,59 +11,71 @@ export default function Login() {
     const [password, setPassword] = useState('');
     const [isLoggedIn, setIsLoggedin] = useState(false);
 
-  handleUserIdChange = name => {
-    setUserName(name);
-  };
-  handlePasswordChange = password => {
-    setPassword(password);
-  };
-  submitclick = () => {
-    if (userName == 'krishankantsinghal' && password == 'krishankant123') {
-      setIsLoggedin(true);
-    }
-    handlePasswordChange = (password) => {
-        setPassword(password)
-    }
+    handleUserIdChange = name => {
+        setUserName(name);
+    };
+    handlePasswordChange = password => {
+        setPassword(password);
+    };
     submitclick = () => {
-        if ((userName == 'krishankantsinghal') && (password == 'krishankant123')) {
+        if (userName == 'krishankantsinghal' && password == 'krishankant123') {
             setIsLoggedin(true);
         }
-    }
-    checkAuth = () => {
-        fetchProfiles()
-            .then( (data) => {
-                let results = data.results;
-                this.setState({profiles: results});
-            });
-    };
+        handlePasswordChange = (password) => {
+            setPassword(password)
+        }
+        submitclick = () => {
+            if ((userName == 'krishankantsinghal') && (password == 'krishankant123')) {
+                setIsLoggedin(true);
+            }
+        }
+        checkAuth = () => {
+            fetchProfiles()
+                .then( (data) => {
+                    let results = data.results;
+                    this.setState({profiles: results});
+                });
+        };
+        submitclick = () => {
+            if (userName == 'krishankantsinghal' && password == 'krishankant123') {
+                setIsLoggedin(true);
+            }
+            checkAuth = () => {
+                fetchProfiles()
+                    .then( (data) => {
+                        let results = data.results;
+                        this.setState({profiles: results});
+                    });
+            };
 
-    return (
-        <View style={loginStyles.loginContainer}>
-            <TextInput 
-                placeholder="Username"
-                onChangeText={this.handleUserIdChange}
-                name="username"
-                style={loginStyles.inputText}
-            />
-            <TextInput 
-                placeholder="Password"
-                onChangeText={this.handlePasswordChange}
-                name="password"
-                style={loginStyles.inputText}
-            />
-            {/* <input type="text" name="username" hint="username" onChange={this.handleUserIdChange} />
+            return (
+                <View style={loginStyles.loginContainer}>
+                    <TextInput 
+                        placeholder="Username"
+                        onChangeText={this.handleUserIdChange}
+                        name="username"
+                        style={loginStyles.inputText}
+                    />
+                    <TextInput 
+                        placeholder="Password"
+                        onChangeText={this.handlePasswordChange}
+                        name="password"
+                        style={loginStyles.inputText}
+                    />
+                    {/* <input type="text" name="username" hint="username" onChange={this.handleUserIdChange} />
             <input type="password" name="password" hint="password" onChange={this.handlePasswordChange} /> */}
-            {/* <button name="submit" onClick={this.submitclick}> Submit</button> */}
-            <TouchableOpacity style={loginStyles.buttonStyle}>
-                <Button type="submit"
-                    onSubmitEditing = {this.checkAuth}
-                    onPress={this.submitclick}
-                    onKeyPress={this.submitclick}
-                    title='LOGIN'
-                    color="#fff"
-                />
-            </TouchableOpacity>
-        </View>
-    );
-};
-  
+                    {/* <button name="submit" onClick={this.submitclick}> Submit</button> */}
+                    <TouchableOpacity style={loginStyles.buttonStyle}>
+                        <Button type="submit"
+                            onSubmitEditing = {this.checkAuth}
+                            onPress={this.submitclick}
+                            onKeyPress={this.submitclick}
+                            title='LOGIN'
+                            color="#fff"
+                        />
+                    </TouchableOpacity>
+                </View>
+            );
+        }
+    }
+}
